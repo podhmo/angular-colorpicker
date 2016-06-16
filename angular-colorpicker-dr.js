25f646380e607fe17a7e9c47d80f7705122116dc
podhmo <podhmo+altair@beproud.jp>
2016-06-16 15:11:24 +0900

    indentation

--- angular-colorpicker-dr.js
+++ angular-colorpicker-dr.js
@@ -20,127 +20,127 @@
 * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
 */
 (function () {
-    'use strict';
-    var directive = function ($compile, $document, $window) {
-        return {
+  'use strict';
+  var directive = function ($compile, $document, $window) {
+    return {
 			restrict: 'A',
-            require: 'ngModel',
-            scope: {
-                changeColor: "&",
+      require: 'ngModel',
+      scope: {
+        changeColor: "&",
 				tinyTrigger: "@",
 				colorMe: "@",
 				ngModel : '='
-            },
+      },
 
-            link: function (scope, element, attrs, ngModel) {
-                var content,
-                    templateHidden = '<div class="color-picker-wrapper body hide">',
-                    templateInline = '<div class="color-picker-wrapper">',
-                    template =
-                            '<div class="color-picker-row-wrapper">' +
-                                '<div class="color-picker-row">' +
-                                    '<cp-color class="color-picker navy" color="#001F3F">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker blue" color="#0074D9">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker aqua" color="#7FDBFF">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker teal" color="#39CCCC">' +
-                                    '</cp-color>' +
-                                '</div>' +
-                            '</div>' +
-                            '<div class="color-picker-row-wrapper">' +
-                                '<div class="color-picker-row">' +
-                                    '<cp-color class="color-picker olive" color="#3D9970">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker green" color="#2ECC40">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker lime" color="#01FF70">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker yellow" color="#FFDC00">' +
-                                    '</cp-color>' +
-                                '</div>' +
-                            '</div>' +
-                            '<div class="color-picker-row-wrapper">' +
-                                '<div class="color-picker-row">' +
-                                    '<cp-color class="color-picker orange" color="#FF851B">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker red" color="#FF4136">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker maroon" color="#85144B">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker fuchia" color="#F012BE">' +
-                                    '</cp-color>' +
-                                '</div>' +
-                            '</div>' +
-                            '<div class="color-picker-row-wrapper">' +
-                                '<div class="color-picker-row">' +
-                                    '<cp-color class="color-picker purple" color="#B10DC9">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker black" color="#111111">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker gray" color="#AAAAAA">' +
-                                    '</cp-color>' +
-                                    '<cp-color class="color-picker white" color="#FFFFFF">' +
-                                    '</cp-color>' +
-                                '</div>' +
-                            '</div>' +
-                        '</div>',
-					templateTinyTrigger =
+      link: function (scope, element, attrs, ngModel) {
+        var content,
+            templateHidden = '<div class="color-picker-wrapper body hide">',
+            templateInline = '<div class="color-picker-wrapper">',
+            template =
+            '<div class="color-picker-row-wrapper">' +
+            '<div class="color-picker-row">' +
+            '<cp-color class="color-picker navy" color="#001F3F">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker blue" color="#0074D9">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker aqua" color="#7FDBFF">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker teal" color="#39CCCC">' +
+            '</cp-color>' +
+            '</div>' +
+            '</div>' +
+            '<div class="color-picker-row-wrapper">' +
+            '<div class="color-picker-row">' +
+            '<cp-color class="color-picker olive" color="#3D9970">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker green" color="#2ECC40">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker lime" color="#01FF70">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker yellow" color="#FFDC00">' +
+            '</cp-color>' +
+            '</div>' +
+            '</div>' +
+            '<div class="color-picker-row-wrapper">' +
+            '<div class="color-picker-row">' +
+            '<cp-color class="color-picker orange" color="#FF851B">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker red" color="#FF4136">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker maroon" color="#85144B">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker fuchia" color="#F012BE">' +
+            '</cp-color>' +
+            '</div>' +
+            '</div>' +
+            '<div class="color-picker-row-wrapper">' +
+            '<div class="color-picker-row">' +
+            '<cp-color class="color-picker purple" color="#B10DC9">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker black" color="#111111">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker gray" color="#AAAAAA">' +
+            '</cp-color>' +
+            '<cp-color class="color-picker white" color="#FFFFFF">' +
+            '</cp-color>' +
+            '</div>' +
+            '</div>' +
+            '</div>',
+					  templateTinyTrigger =
 						'<div class="color-picker-wrapper picker-icon">' +
 						'<div class="color-picker-row-wrapper">' +
-							'<div class="color-picker-row">' +
-								'<cp-color class="color-picker navy">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker blue">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker aqua">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker teal">' +
-								'</cp-color>' +
-							'</div>' +
+						'<div class="color-picker-row">' +
+						'<cp-color class="color-picker navy">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker blue">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker aqua">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker teal">' +
+						'</cp-color>' +
+						'</div>' +
 						'</div>' +
 						'<div class="color-picker-row-wrapper">' +
-							'<div class="color-picker-row">' +
-								'<cp-color class="color-picker olive">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker green">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker lime">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker yellow">' +
-								'</cp-color>' +
-							'</div>' +
+						'<div class="color-picker-row">' +
+						'<cp-color class="color-picker olive">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker green">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker lime">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker yellow">' +
+						'</cp-color>' +
+						'</div>' +
 						'</div>' +
 						'<div class="color-picker-row-wrapper">' +
-							'<div class="color-picker-row">' +
-								'<cp-color class="color-picker orange">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker red">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker maroon">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker fuchia">' +
-								'</cp-color>' +
-							'</div>' +
+						'<div class="color-picker-row">' +
+						'<cp-color class="color-picker orange">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker red">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker maroon">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker fuchia">' +
+						'</cp-color>' +
+						'</div>' +
 						'</div>' +
 						'<div class="color-picker-row-wrapper">' +
-							'<div class="color-picker-row">' +
-								'<cp-color class="color-picker purple">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker black">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker gray">' +
-								'</cp-color>' +
-								'<cp-color class="color-picker white">' +
-								'</cp-color>' +
-							'</div>' +
+						'<div class="color-picker-row">' +
+						'<cp-color class="color-picker purple">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker black">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker gray">' +
+						'</cp-color>' +
+						'<cp-color class="color-picker white">' +
+						'</cp-color>' +
+						'</div>' +
 						'</div>' +
 						'</div>',
-					elementTinyTrigger,
-					elementColorPicker,
-					closestFunc,
-					prevFunc;
+					  elementTinyTrigger,
+					  elementColorPicker,
+					  closestFunc,
+					  prevFunc;
 				prevFunc = function (elem) {
 					if (elem.previousElementSibling) {
 						return elem.previousElementSibling;
@@ -175,8 +175,8 @@
 
 					elementTinyTrigger.bind("click", function (ev) {
 						var wrapper = closestFunc(angular.element(ev.target), 'color-picker-wrapper'),
-							top = wrapper[0].getBoundingClientRect().top,
-							height = wrapper[0].getBoundingClientRect().height;
+							  top = wrapper[0].getBoundingClientRect().top,
+							  height = wrapper[0].getBoundingClientRect().height;
 						top = top + $window.pageYOffset;
 
 						elementColorPicker.removeClass('hide');
@@ -204,7 +204,7 @@
 						element.bind("click", function (ev) {
 							//show color picker beneath the input
 							var top = ev.target.getBoundingClientRect().top,
-								height = ev.target.getBoundingClientRect().height;
+								  height = ev.target.getBoundingClientRect().height;
 							top = top + $window.pageYOffset;
 
 							elementColorPicker.removeClass('hide');
@@ -218,9 +218,9 @@
 						elementTinyTrigger.bind("click", function (ev) {
 							//show color picker beneath the input
 							var $wrapper = closestFunc(angular.element(ev.target), 'color-picker-wrapper'),
-								wrapperPrev = prevFunc($wrapper[0]),
-								top = wrapperPrev.getBoundingClientRect().top,
-								height = wrapperPrev.getBoundingClientRect().height;
+								  wrapperPrev = prevFunc($wrapper[0]),
+								  top = wrapperPrev.getBoundingClientRect().top,
+								  height = wrapperPrev.getBoundingClientRect().height;
 							top = top + $window.pageYOffset;
 
 							elementColorPicker.removeClass('hide');
@@ -244,15 +244,15 @@
 				//when clicking somewhere on the screen / body -> hide the color picker
 				$document.bind("click", function (ev) {
 					var i,
-						docChildren = $document.find('body').children();
+						  docChildren = $document.find('body').children();
 					for (i = 0; i < docChildren.length; i++) {
 						if (docChildren[i].className.indexOf('color-picker-wrapper body') !== -1) {
 							angular.element(docChildren[i]).addClass('hide');
 						}
 					}
 				});
-	        }
-        };
+	    }
     };
-    angular.module('colorpicker-dr', []).directive('colorPicker', directive);
+  };
+  angular.module('colorpicker-dr', []).directive('colorPicker', directive);
 }());
